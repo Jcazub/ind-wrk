bb62731ab65e65e3905769f79259b5d86d369978
jcazub <jcazub@gmail.com>
2017-12-06 13:20:07 -0500

    finished milestone 1 exercise 3

---
+++ netBeans Java Projects/softwareguild/MilestoneOne/ExerciseFour/src/main/java/com/sg/exercise/four/ifelse/KnockKnock.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.exercise.four.ifelse;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jesse
+ */
+public class KnockKnock {
+     public static void main(String[] args) {
+
+        Scanner inputReader = new Scanner(System.in);
+
+        System.out.print("Knock Knock! Guess who!! ");
+        String nameGuess = inputReader.nextLine();
+
+        // nameGuess == "Marty McFly" doesn't work because == is not used to compare strings
+        // The string is also 
+        if(nameGuess.equals("Marty McFly")){
+            System.out.println("Hey! That's right! I'm back!");
+            System.out.println(".... from the Future."); // Sorry, had to!
+        }else{
+            System.out.println("Dude, do I -look- like " + nameGuess);
+        }
+    }
+}
--- netBeans Java Projects/softwareguild/MilestoneOne/ExerciseFour/src/main/java/com/sg/exercise/four/ifelse/TriviaNight.java
+++ netBeans Java Projects/softwareguild/MilestoneOne/ExerciseFour/src/main/java/com/sg/exercise/four/ifelse/TriviaNight.java
@@ -13,15 +13,64 @@ import java.util.Scanner;
  */
 public class TriviaNight {
     public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        int answersRight = 0;
+        
         System.out.println("It's TRIVIA NIGHT! Are you ready?!");
         System.out.println("");
         System.out.println("FIRST QUESTION!");
         System.out.println("What is the Lowest Level Programming Language?");
         System.out.println("1) Soure Code           2) Assembly Language");
         System.out.println("2) C#                   3) Machine Code");
+         
+        String responeOneInput = myScanner.nextLine();
+        int responseOne = Integer.parseInt(responeOneInput);
         
-        Scanner myScanner = new Scanner(System.in);
+        System.out.println("");
+        System.out.println("YOUR ANSWER: " + responseOne);
+        System.out.println("");
         
+        System.out.println("SECOND QUESTION!");
+        System.out.println("Website Security CAPTCHA Forms Are Descended From the Work of?");
+        System.out.println("1) Grace Hopper         2) Alan Turing");
+        System.out.println("2) Charles Babbage      3) Larry Page");
+        
+        String responeTwoInput = myScanner.nextLine();
+        int responseTwo = Integer.parseInt(responeTwoInput);
+        
+        System.out.println("");
+        System.out.println("YOUR ANSWER: " + responseTwo);
+        System.out.println("");
+        
+        System.out.println("LAST QUESTION!");
+        System.out.println("Which of These Sci-Fi Ships Was Once Slated for a Full-Size Replica in Las Vegas?");
+        System.out.println("1) Serenity             2) The Battlestar Galactica");
+        System.out.println("2) The USS Enterprise   3) The Millennium Falcon");
+        
+        String responeThreeInput = myScanner.nextLine();
+        int responseThree = Integer.parseInt(responeThreeInput);
+        
+        System.out.println("");
+        System.out.println("YOUR ANSWER: " + responseThree);
+        System.out.println("");
+        
+        if (responseOne == 4) {
+            answersRight++;
+        } if (responseTwo == 2) {
+            answersRight++;
+        } if (responseThree == 3) {
+            answersRight++;
+        }
+        
+        if (answersRight == 3) {
+            System.out.println("AWESOME!!! You got all of 'em right!");
+        } if (answersRight == 2){
+            System.out.println("Good job! You got 2 right!");
+        } if (answersRight == 1) {
+            System.out.println("One right isn't too bad!");
+        } if (answersRight == 0) {
+            System.out.println("You gotta brush up on your nerd history! You didn't get any answers right!");
+        }
         
     }
 }
