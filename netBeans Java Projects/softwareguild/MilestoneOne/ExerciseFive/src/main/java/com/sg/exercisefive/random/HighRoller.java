33570d0a14a223d529c0e71f2550d87eade75c5a
jcazub <jcazub@gmail.com>
2017-12-06 19:53:39 -0500

    Had to fix file structure as some projects were not in folder tracked by git. Finished Milstone 1 exercise 5. Started Milestone 1, exercise 6.

---
+++ netBeans Java Projects/softwareguild/MilestoneOne/ExerciseFive/src/main/java/com/sg/exercisefive/random/HighRoller.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.exercisefive.random;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jesse
+ */
+public class HighRoller {
+     public static void main(String[] args) {
+
+        Random diceRoller = new Random();
+        Scanner myScanner = new Scanner(System.in);
+        
+        System.out.print("Hey there, adventurer. How many sides does your dice have?: ");
+        String diceInput = myScanner.nextLine();
+        int dice = Integer.parseInt(diceInput);
+         System.out.println("A "+dice+" sided die? Nice.");
+        
+        int rollResult = diceRoller.nextInt(dice) + 1;
+
+        System.out.println("TIME TO ROOOOOOLL THE DICE!");
+        System.out.println("I rolled a " + rollResult);
+
+        if (rollResult == 1) {
+            System.out.println("You rolled a critical failure! Ouch.");
+        } else if (rollResult == dice) {
+            System.out.println("You rolled a critical! Nice Job!");
+        } 
+    }
+}
---
+++ netBeans Java Projects/softwareguild/MilestoneOne/ExerciseSix/src/main/java/com/sg/exercisesix/whilesAndDos/StayPositive.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.exercisesix.whilesAndDos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jesse
+ */
+public class StayPositive {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("What number should I count down from?: ");
+        String numberInput = myScanner.nextLine();
+        int number = Integer.parseInt(numberInput);
+        int count = 0;
+        
+        System.out.println("Here goes!");
+        System.out.println("");
+        
+        while (number >= 0) {
+            System.out.print(number+" ");
+             if (count == 9){
+               count = -1;
+               System.out.println("");
+            }
+            number--;
+            count++;
+        }
+        System.out.println("");
+        System.out.println("Whew, better stop there...!");
+    }
+  
+}
